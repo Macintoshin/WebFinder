2a8703fd77ed766dfe4a6dd276a7edf2914d4088
Jordan Lamp <80288917+Macintoshin@users.noreply.github.com>
2022-12-13 19:34:54 -0500

    update

--- search.js
+++ search.js
@@ -6,38 +6,40 @@ function googleSearch()
 
     window.location.href=url;
 }
-function search_func(){
-address=document.getElementById("Query").value;
-}
-
-function handle(e){
-address=document.getElementById("Query").value;
-if(e.keyCode === 13){
-var text=document.getElementById("Query").value;
+function wibySearch()
+{
+    var text=document.getElementById("Query").value;
     var cleanQuery = text.replace(" ","+",text);
-    var url='http://www.google.com/search?q='+cleanQuery;
+    var url='https://wiby.me/?q='+cleanQuery;
 
     window.location.href=url;
 }
-return false;
+function classicSearch()
+{
+    var url='classicindex.html';
+
+    window.location.href=url;
 }
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    googleSearch()
-  }
-});
+function modernSearch()
+{
+    var url='index.html';
+
+    window.location.href=url;
+}
+function search_func(){
+address=document.getElementById("Query").value;
+}
 function changeBackground() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
 
 // using inline variable
       document.getElementById("logo").style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
-      document.getElementById("logo").style.border = "solid 2px black"
+      document.getElementById("logo").style.border = "solid 2px #636498"
       document.getElementById("mac").style.padding = "1vmin 1vmin 1vmin 1vmin"
-      document.getElementById("mac").style.borderRadius = "5px"
+
       document.getElementById("name").style.padding = "1vmin 1vmin 1vmin 1vmin"
-      document.getElementById("name").style.borderRadius = "5px"
       
 
     // using local variable
